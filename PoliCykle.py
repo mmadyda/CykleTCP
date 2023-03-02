588bb7d6fd878ef75310637ba75c780153cc8fe9
mmadyda <63304339+mmadyda@users.noreply.github.com>
2023-03-02 11:10:11 +0100

    ssasa

--- PoliCykle.py
+++ PoliCykle.py
@@ -15,12 +15,10 @@ if os.path.exists(os.path.join(cwd, 'data', 'poli.ini')):
 
 
     liczba_maszyn = int(config['DEFAULT']['liczba_maszyn'])  # liczba obslugiwanych maszyn
-    folder_startowy = str(config['DEFAULT']['folder_startowy'])
     folder_programu = str(config['DEFAULT']['folder_programu'])
     nazwa_programu = str(config['DEFAULT']['nazwa_programu'])
 else:
     liczba_maszyn = 1
-    folder_startowy = 'PoliCykle'
     folder_programu = 'cykle'
     nazwa_programu = 'app'
 
@@ -28,7 +26,7 @@ else:
 
 
 for i in range(1,liczba_maszyn+1):
-    path = os.path.join(folder_startowy, 'cykle'+str(i), 'app'+str(i)+'.exe')
+    path = os.path.join('cykle'+str(i), 'app'+str(i)+'.exe')
     print(path)
     os.startfile(path)
 
